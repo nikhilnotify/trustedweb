41fa8dd229b4facd564b45092c9a431247bef03b
nikhil.t <nikhil.t@notifyvisitors.com>
2019-04-09 16:11:19 +0530

    test2

--- main.php
+++ main.php
@@ -1,6 +1,6 @@
 <pre>
 <?php
-    $a = $_SERVER['REQUEST_SCHEME'] . '://' . $_SERVER['SERVER_NAME'];
+    $a = $_SERVER['REQUEST_SCHEME'] . 's://' . $_SERVER['SERVER_NAME'];
 ?>
 <!-- <a href="<?=$a?>"> home </a>  <a href="<?=$a?>/trustedweb/product/one/"> one index </a>  <a href="<?=$a?>/trustedweb/product/one/one.php"> one.php </a>  <a href="<?=$a?>/trustedweb/product/two/"> two index </a> <a href="<?=$a?>/trustedweb/product/two/two.php"> two.php </a>   -->
 
@@ -8,23 +8,4 @@
 
 
 
-<?php
-
-
-echo __FILE__;
-echo "<br>";
-echo "<br>";
-echo "<br>";
-
-
-echo __DIR__;
-echo "<br>";
-echo "<br>";
-echo "<br>";
-
-
-echo __LINE__;
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
+<h1><?=$_SERVER['QUERY_STRING']?></h1>
