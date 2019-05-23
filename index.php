303f32b94e6944b17cdc6a68d96662f00debe299
nikhilnotify <49435223+nikhilnotify@users.noreply.github.com>
2019-05-23 11:35:21 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
 <?php
 
-    include_once 'main.php';
+    // include_once 'main.php';
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hi there</title>
+	<link rel="manifest" href="https://dev17.notifyvisitors.com/ext/v1/pwa/1284/b97cz0vv">
+</head>
+<body>
+
+</body>
+</html>
