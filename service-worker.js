50815ad00cdd6d5cfbee206b8a2689786fd64efd
nikhilnotify <49435223+nikhilnotify@users.noreply.github.com>
2019-05-31 10:38:53 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,2 +1,17 @@
-var version = '2.2.1';
-importScripts('js/service-worker.js');
+var version = '0.1';
+var amp_version = '0.1';
+var NOTIFYVISITORS_BRAIND_ID = '6022';
+var _nvClientConfig = {
+    auth: {
+        bid: '6022',
+        bid_e: '0960BCBB47912207E789499C9DCBF95D',
+        t: "420"
+    },
+    data: {},
+    cPubKey: '',
+}
+
+importScripts('https://cdnp.notifyvisitors.com/js/brand_hosted/amp-push-worker.js');
+importScripts('https://cdnp.notifyvisitors.com/js/brand_hosted/push-worker.js');
+importScripts('https://s3.amazonaws.com/notifypush/cache_worker/config-6022.js');
+importScripts('https://cdnp.notifyvisitors.com/js/brand_hosted/cache-worker.js');
